003bb1981e6619a200b3119b1eb9e848a792b143
Robert-Jung <rjinjung@gmail.com>
2017-03-17 15:07:14 -0700

    User can see number products in cart and return to landing page #10

--- travelzon/main.js
+++ travelzon/main.js
@@ -3,28 +3,28 @@ var app = [
     cart = [],
     travelPackage = [
     {
-    id: 1,
+    id: '1',
     destination: 'Location 1',
     price: 1111,
     img: '1.jpg',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel pulvinar nibh. Suspendisse lobortis, ipsum eget vulputate rutrum, erat sem facilisis lectus, tempor vehicula augue neque at tortor.'
     },
     {
-    id: 2,
+    id: '2',
     destination: 'Location 2',
     price: 2222,
     img: '2.jpg',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel pulvinar nibh. Suspendisse lobortis, ipsum eget vulputate rutrum, erat sem facilisis lectus, tempor vehicula augue neque at tortor.'
     },
     {
-    id: 3,
+    id: '3',
     destination: 'Location 3',
     price: 3333,
     img: '3.jpg',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel pulvinar nibh. Suspendisse lobortis, ipsum eget vulputate rutrum, erat sem facilisis lectus, tempor vehicula augue neque at tortor.'
     },
     {
-    id: 4,
+    id: '4',
     destination: 'Location 4',
     price: 4444,
     img: '4.jpg',
@@ -35,19 +35,20 @@ var app = [
 var $wrapper = document.createElement('div')
 document.body.appendChild($wrapper).classList.add('container')
 
-//nav bar for home return and cart
+//nav bar cart
 var $nav = document.createElement('nav')
 $wrapper.appendChild($nav).classList.add('navbar', 'navbar-inverse')
 
 var $navCart = document.createElement('span')
 $nav.appendChild($navCart).classList.add('glyphicon', 'glyphicon-shopping-cart')
 
+//nav bar home
 var $homeBtn = document.createElement('button')
 $homeBtn.id = 'home'
 $homeBtn.textContent = 'HOME'
 $nav.appendChild($homeBtn).classList.add('btn', 'btn-default')
 
-// Rows
+//deals
 var $wrapperRow = document.createElement('div')
 $wrapper.appendChild($wrapperRow).classList.add('row')
 
@@ -57,13 +58,14 @@ function render(packages) {
     var $img = document.createElement('img')
     var $p = document.createElement('p')
     var $desc = document.createElement('div')
-    var $button = document.createElement('div')
+    var $button = document.createElement('button')
 
     $img.setAttribute('src', travelPackage[i].img)
     $desc.textContent = travelPackage[i].description
     $button.textContent = ('Book a trip to ' + travelPackage[i].destination + ' for $' + travelPackage[i].price)
 
     $p.id = travelPackage[i].destination
+    $button.id = travelPackage[i].id
 
     $wrapperRow.appendChild($div).classList.add('container', 'col-md-6', 'spacing', 'imgoverlay')
     $div.appendChild($img).classList.add('img-responsive', 'resize')
@@ -74,7 +76,7 @@ function render(packages) {
     return $div
 }
 
-var $oneDeal = render()
+var $oneDeal = render(travelPackage)
 $wrapperRow.appendChild($oneDeal)
 
 //single page description
@@ -85,10 +87,11 @@ function renderSingle(deal) {
   var $img = document.createElement('img')
   var $desc = document.createElement('div')
   var $purchase = document.createElement('div')
-  var $button = document.createElement('div')
+  var $button = document.createElement('button')
 
   $img.setAttribute('src', deal.img)
   $desc.textContent = deal.description
+  $button.id = deal.id
   $button.textContent = ('Book a trip to ' + deal.destination + ' for $' + deal.price)
 
   $wrapperRow.appendChild($div).classList.add('container', 'spacing')
@@ -100,8 +103,8 @@ function renderSingle(deal) {
   $purchase.appendChild($button).classList.add('btn', 'btn-info')
 }
 
-function singlePage (event) {
-  for (var i = 0; i < travelPackage.length;i++) {
+function singlePage(event) {
+  for (var i = 0; i < travelPackage.length; i++) {
     if (event.target.id === travelPackage[i].destination) {
       $wrapperRow.innerHTML = ""
       renderSingle(travelPackage[i])
@@ -113,5 +116,25 @@ $wrapperRow.addEventListener('click', singlePage)
 
 //add to cart
 var $cartDisplay = document.createElement('span')
-$cartDisplay.textContent = cart.length
-$nav.appendChild($cartDisplay).classList.add('cartDisplay')
+
+function cartBtn(addClick) {
+  for (var i = 0; i < travelPackage.length; i++) {
+    if (addClick.target.id === travelPackage[i].id) {
+      cart.push(travelPackage[i])
+      $cartDisplay.textContent = cart.length
+      $nav.appendChild($cartDisplay).classList.add('cartDisplay')
+    }
+  }
+}
+
+$wrapperRow.addEventListener('click', cartBtn)
+
+//return to home
+var homePage = document.getElementById('home')
+
+function backHome() {
+  $wrapperRow.innerHTML = ""
+  render(travelPackage)
+}
+
+homePage.addEventListener('click', backHome)
